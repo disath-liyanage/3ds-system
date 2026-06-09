ef34b222ef7bb28afb5398b5f4b78c452fbf9d1a
Disath Liyanage <disathsenitha@gmail.com>
2026-06-10 02:15:57 +0530

    tab name

---
+++ apps/web/src/app/(dashboard)/admin/backup/layout.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Backup"
+};
+
+export default function AttendanceLayout({ children }: { children: React.ReactNode }) {
+  return children;
+}
